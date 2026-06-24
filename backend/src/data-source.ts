5a877b349e94ec8da567b6933d5fc06b01c130e9
Ankit Kumar Singh <ankit.ks@celestialsys.com>
2026-06-24 13:06:30 +0530

    made changes in DataSource connection

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -6,7 +6,9 @@ dotenv.config()
 export const AppDataSource = new DataSource({
      type: "postgres",
      url: process.env.DATABASE_URL!,
-     ssl: true,
+     ssl: {
+        rejectUnauthorized: true,
+    },
      synchronize: true,
      entities: [Event],
 }); 
