327f94c670741a1a75ceea8237f1b5970761c898
Ankit Kumar Singh <ankit.ks@celestialsys.com>
2026-06-24 15:50:33 +0530

    Removed dist folder

--- backend/dist/entities/Event.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Event {
-    id: number;
-    title: string;
-    summary: string;
-    date: Date;
-    start_time: string;
-    end_time: string;
-}
-//# sourceMappingURL=Event.d.ts.map
