8e2e3a437e60aa7f81b44374627ccdd35c6898f8
Ankit Kumar Singh <ankit.ks@celestialsys.com>
2026-06-24 15:56:15 +0530

    Made changes in the folder

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,19 +9,14 @@ import { resolvers } from './graphql/resolvers.js';
 
 const startServer = async () => {
   try {
-    console.log("DATABASE_URL =", process.env.DATABASE_URL);
     await AppDataSource.initialize();
     console.log('Database connected successfully');
-
     const app = express();
-
     const server = new ApolloServer({
       typeDefs,
       resolvers,
     });
-
     await server.start();
-
     app.use(
       '/graphql',
       cors({
